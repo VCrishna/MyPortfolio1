9d5e0f814368daece282a52a5ed794f2a0dfd195
VCrishna <vskrishna5558@gmail.com>
2023-10-12 22:38:27 -0400

    Final Changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./registerServiceWorker"; // Updated this line
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // registerServiceWorker();
-unregister();
+serviceWorker.unregister();
